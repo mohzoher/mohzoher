f8eb01c598412fcc31bbe4c7d15a40eedd8d2eb7
Mohammad <mohzoheruk@gmail.com>
2024-04-29 18:44:51 +0100

    Yes

--- little/App.js
+++ little/App.js
@@ -195,16 +195,32 @@ const styles = StyleSheet.create({
 
 // const Drawer = createDrawerNavigator();
 
+// import { View, StyleSheet } from 'react-native';
+// import LittleLemonHeader from './components/LittleLemonHeader';
+// import LittleLemonFooter from './components/LittleLemonFooter';
+// import WelcomeScreen from './WelcomeScreen';
+// import LoginScreen from './LoginScreen';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createDrawerNavigator } from '@react-navigation/drawer';
+
+// const Drawer = createDrawerNavigator();
+
 // export default function App() {
 //   return (
 //     <>
-//     <NavigationContainer>
-//       <Drawer.Navigator>
-//         <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-//         <Drawer.Screen name="Menu" component={MenuScreen} />
-//       </Drawer.Navigator>
-//     </NavigationContainer>
-//    </>
+//       <NavigationContainer>
+//         <View style={styles.container}>
+//           <LittleLemonHeader />
+//           <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+//             <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+//             <Drawer.Screen name="Login" component={LoginScreen} />
+//           </Drawer.Navigator>
+//         </View>
+//         <View style={styles.footerContainer}>
+//           <LittleLemonFooter />
+//         </View>
+//       </NavigationContainer>
+//     </>
 //   );
 // }
 
@@ -214,5 +230,4 @@ const styles = StyleSheet.create({
 //     backgroundColor: '#333333',
 //   },
 //   footerContainer: { backgroundColor: '#333333' },
-
 // });
